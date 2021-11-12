863cda5fb23be3d0e5f39821a09e59520111723e
Kevin Thomas <kevin@mytechnotalent.com>
2021-11-12 11:04:25 -0500

    Update MicroCipher.py

--- MicroCipher.py
+++ MicroCipher.py
@@ -24,61 +24,55 @@
 # SOFTWARE.
 
 
-class MicroEnigma:
+class MicroCipher:
     """
-    Class to handle the MicroEnigma engine
+    Class to handle the MicroCipher engine
     """
     
-    def __init__(self, fast_rotor, medium_rotor, slow_rotor):
+    def __init__(self, fast_wheel, medium_wheel, slow_wheel):
         """
         Attrs:
-            fast_rotor: int
-            medium_rotor: int
-            slow_rotor: int
+            fast_wheel: int
+            medium_wheel: int
+            slow_wheel: int
         """
-         # special thanks to Bradan Lane for the correct rotor and reflector b mappings, check out his Pocket Enigma @ https://www.tindie.com/products/bradanlane/pocket-enigma
-        self.fast_rotor_alphabet = ['E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J']
-        self.medium_rotor_alphabet = ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E']
-        self.slow_rotor_alphabet = ['B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O']
-        self.reflector_b_alphabet = ['Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T']
+        self.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
         # computers are zero-indexed, we need to subtract 1, if user selects 1 it becomes 0
-        self.fast_rotor = fast_rotor - 1
-        self.medium_rotor = medium_rotor - 1
-        self.slow_rotor = slow_rotor - 1
-        rotors = [self.fast_rotor, self.medium_rotor, self.slow_rotor]
-        for rotor in rotors:
-            rotor = rotor % 26
-        self.fast_rotor = rotors[0]
-        self.medium_rotor = rotors[1]
-        self.slow_rotor = rotors[2]
-        self.reflector = [letter for letter in reversed(self.reflector_b_alphabet)]
+        self.fast_wheel = fast_wheel - 1
+        self.medium_wheel = medium_wheel - 1
+        self.slow_wheel = slow_wheel - 1
+        wheels = [self.fast_wheel, self.medium_wheel, self.slow_wheel]
+        for wheel in wheels:
+            wheel = wheel % 26
+        self.fast_wheel = wheels[0]
+        self.medium_wheel = wheels[1]
+        self.slow_wheel = wheels[2]
+        self.reverse_wheel = [letter for letter in reversed(self.alphabet)]
         
-    def permutate(self, rotor, alphabet):
+    def permutate(self, wheel):
         """
-        Method to re-order the alphabet depending on the rotors settings
+        Method to re-order the alphabet depending on the wheels settings
         
         Params:
-            rotor: int
-            alphabet: list
+            wheel: int
         """
-        new_alphabet = ''.join(alphabet)
+        new_alphabet = ''.join(self.alphabet)
         new_alphabet = list(new_alphabet)
-        for iter in range(rotor):
+        for iter in range(wheel):
             new_alphabet.insert(0, new_alphabet[-1])
             new_alphabet.pop(-1)
         return new_alphabet
         
-    def inverse_permutation(self, rotor, alphabet):
+    def inverse_permutation(self, wheel):
         """
-        Method to inverse re-order the alphabet depending on the rotors settings
+        Method to inverse re-order the alphabet depending on the wheels settings
         
         Params:
-            rotor: int
-            alphabet: list
+            wheel: int
         """
-        new_alphabet = ''.join(alphabet)
+        new_alphabet = ''.join(self.alphabet)
         new_alphabet = list(new_alphabet)
-        for iter in range(rotor):
+        for iter in range(wheel):
             new_alphabet.append(new_alphabet[0])
             new_alphabet.pop(0)
         return new_alphabet
@@ -94,35 +88,35 @@ class MicroEnigma:
         text.split()
         # cipher each letter
         for letter in text:
-            # handle empty space to ignore rotor algorithm
+            # handle empty space to ignore wheel algorithm
             if letter == ' ':
                 cipher_text.append(' ')
             else:
-                # letter is ciphered by fast rotor
-                temp_letter = self.permutate(self.fast_rotor, self.fast_rotor_alphabet)[self.fast_rotor_alphabet.index(letter)]
-                # letter is ciphered by medium rotor
-                temp_letter = self.permutate(self.medium_rotor, self.medium_rotor_alphabet)[self.medium_rotor_alphabet.index(temp_letter)]
-                # letter is ciphered by slow rotor
-                temp_letter = self.permutate(self.slow_rotor, self.slow_rotor_alphabet)[self.slow_rotor_alphabet.index(temp_letter)]
-                # reflector is returning the inverse of that letter
-                temp_letter = self.reflector[self.reflector_b_alphabet.index(temp_letter)]
-                # letter is ciphered by slow rotor, inverse algorithm
-                temp_letter = self.inverse_permutation(self.slow_rotor, self.slow_rotor_alphabet)[self.slow_rotor_alphabet.index(temp_letter)]
-                # letter is ciphered by medium rotor, inverse algorithm
-                temp_letter = self.inverse_permutation(self.medium_rotor, self.medium_rotor_alphabet)[self.medium_rotor_alphabet.index(temp_letter)]
-                # letter is ciphered by fast rotor, inverse algorithm
-                temp_letter = self.inverse_permutation(self.fast_rotor, self.fast_rotor_alphabet)[self.fast_rotor_alphabet.index(temp_letter)]
+                # letter is ciphered by fast wheel
+                temp_letter = self.permutate(self.fast_wheel)[self.alphabet.index(letter)]
+                # letter is ciphered by medium wheel
+                temp_letter = self.permutate(self.medium_wheel)[self.alphabet.index(temp_letter)]
+                # letter is ciphered by slow wheel
+                temp_letter = self.permutate(self.slow_wheel)[self.alphabet.index(temp_letter)]
+                # reverse_wheel is returning the inverse of that letter
+                temp_letter = self.reverse_wheel[self.alphabet.index(temp_letter)]
+                # letter is ciphered by slow wheel, inverse algorithm
+                temp_letter = self.inverse_permutation(self.slow_wheel)[self.alphabet.index(temp_letter)]
+                # letter is ciphered by medium wheel, inverse algorithm
+                temp_letter = self.inverse_permutation(self.medium_wheel)[self.alphabet.index(temp_letter)]
+                # letter is ciphered by fast wheel, inverse algorithm
+                temp_letter = self.inverse_permutation(self.fast_wheel)[self.alphabet.index(temp_letter)]
                 # append temp letter to cipher text
                 cipher_text.append(temp_letter)
-            # rotor rotation algorithm
-            self.fast_rotor += 1
-            if self.fast_rotor % len(self.fast_rotor_alphabet) == 0:
-                self.medium_rotor += 1
-                self.fast_rotor = 0
-            if self.medium_rotor % len(self.medium_rotor_alphabet) == 0 and self.fast_rotor % len(self.fast_rotor_alphabet) != 0 and self.medium_rotor >= len(
-                    self.medium_rotor_alphabet) - 1:
-                self.slow_rotor += 1
-                self.medium_rotor = 1
+            # wheel rotation algorithm
+            self.fast_wheel += 1
+            if self.fast_wheel % len(self.alphabet) == 0:
+                self.medium_wheel += 1
+                self.fast_wheel = 0
+            if self.medium_wheel % len(self.alphabet) == 0 and self.fast_wheel % len(self.alphabet) != 0 and self.medium_wheel >= len(
+                    self.alphabet) - 1:
+                self.slow_wheel += 1
+                self.medium_wheel = 1
         upper_cipher_text = '' 
         for letter in cipher_text: 
             upper_cipher_text += letter  
@@ -131,25 +125,25 @@ class MicroEnigma:
 
 while True:
     try:
-        fast_rotor = int(input('FAST ROTOR: '))
-        if fast_rotor < 1 or fast_rotor > 26:
+        fast_wheel = int(input('FAST WHEEL: '))
+        if fast_wheel < 1 or fast_wheel > 26:
             print('Please enter a value from 1 to 26...')
             break
-        medium_rotor = int(input('MEDIUM ROTOR: '))
-        if medium_rotor < 1 or medium_rotor > 26:
+        medium_wheel = int(input('MEDIUM WHEEL: '))
+        if medium_wheel < 1 or medium_wheel > 26:
             print('Please enter a value from 1 to 26...')
             break
-        slow_rotor = int(input('SLOW ROTOR: '))
-        if slow_rotor < 1 or slow_rotor > 26:
+        slow_wheel = int(input('SLOW WHEEL: '))
+        if slow_wheel < 1 or slow_wheel > 26:
             print('Please enter a value from 1 to 26...')
             break
-        micro_enigma = MicroEnigma(fast_rotor, medium_rotor, slow_rotor)
+        micro_cipher = MicroCipher(fast_wheel, medium_wheel, slow_wheel)
         cipher = input('CIPHER: ')
         if not cipher.isupper():
             print('Please enter only capital letters...')
             break
         print('CIPHER: ', end='')
-        print(micro_enigma.cipher(cipher))
+        print(micro_cipher.cipher(cipher))
     except ValueError:
         print('Please enter a value from 1 to 26...')
         break
